4afb1f9ada79b0491578eb81eefc4318146250c7
hashmatcs <hashmatcs4@gmail.com>
2024-09-30 23:43:17 +0500

    dev

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 const stripeRoutes=require('./routes/stripe.route');
 const cors=require('cors')
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000; 
